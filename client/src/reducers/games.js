768f39ee47ecd272eb098a1f10176fce21e320db
quietQuiet <j.lietunovas@gmail.com>
2019-02-21 20:40:48 +0100

    working comments

---
+++ client/src/reducers/games.js
@@ -0,0 +1,41 @@
+import {ADD_GAME, UPDATE_GAME, UPDATE_GAMES, GAME_ENDED_SUCCESS} from '../actions/games'
+import {USER_LOGOUT} from '../actions/users'
+
+/*
+The state will contain the games in an object with the game ID as key
+*/
+
+export default (state = null, {type, payload}) => {
+  console.log(type)
+  switch (type) {
+    case USER_LOGOUT:
+      return null
+    
+    case ADD_GAME:
+      return {
+        ...state,
+        [payload.id]: payload
+      }
+
+    case UPDATE_GAME:
+      return {
+        ...state,
+        [payload.id]: payload
+      }
+
+    case GAME_ENDED_SUCCESS:
+      return {
+        ...state,
+        [payload.id]: payload
+      }
+
+    case UPDATE_GAMES:
+      return payload.reduce((games, game) => {
+        games[game.id] = game
+        return games
+      }, {})
+
+    default:
+      return state
+  }
+}
---
+++ server/src/games/entities.ts
@@ -0,0 +1,58 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
+import User from '../users/entity'
+
+export type Symbol = 'flo' | 'b'
+export type Row = [ 
+  Symbol | null, 
+  Symbol | null, 
+  Symbol | null ]
+export type Board = [ Row, Row, Row ]
+
+type Status = 'pending' | 'started' | 'finished'
+
+const emptyRow: Row = [null, null, null]
+const emptyBoard: Board = [ emptyRow, emptyRow, emptyRow ]
+
+@Entity()
+export class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('json', {default: emptyBoard})
+  board: Board
+
+  @Column('text', {default: 'flo'})
+  turn: Symbol
+
+  @Column('text', {nullable: true})
+  winner: Symbol
+
+  @Column('text', {default: 'pending'})
+  status: Status
+
+  // this is a relation, read more about them here:
+  // http://typeorm.io/#/many-to-one-one-to-many-relations
+  @OneToMany(_ => Player, player => player.game, {eager:true})
+  players: Player[]
+}
+
+@Entity()
+@Index(['game', 'user', 'symbol'], {unique:true})
+export class Player extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @ManyToOne(_ => User, user => user.players)
+  user: User
+
+  @ManyToOne(_ => Game, game => game.players)
+  game: Game
+
+  @Column('text')
+  symbol: Symbol
+
+  @Column('integer', { name: 'user_id' })
+  userId: number
+}
